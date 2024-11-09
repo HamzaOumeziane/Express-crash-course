455db4506cb28b5769958aa0f50d071c9b64128c
Hamza Oumeziane <2226562@bdeb.qc.ca>
2024-11-09 11:30:32 -0500

    put method

--- routes/posts.js
+++ routes/posts.js
@@ -50,6 +50,18 @@ router.post('/', (req, res) => {
     res.status(201).json(posts);
 })
 
+router.put('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => post.id === id);
+
+    if(!post){
+        return res.status(404).json({msg: `A post with id ${id} does not exist`});
+    }
+
+    post.title = req.body.title
+    res.status(200).json(posts);
+})
+
 export default router;
 
 
