f478d813a6167e23008d94a7a4f1884145e17afb
Hamza Oumeziane <2226562@bdeb.qc.ca>
2024-11-09 11:00:42 -0500

    Putting code in specific routes and use it on server

---
+++ routes/posts.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+let posts = [
+    {id: 1, title: 'Post 1', content: 'This is post 1'},
+    {id: 2, title: 'Post 2', content: 'This is post 2'},
+    {id: 3, title: 'Post 3', content: 'This is post 3'}
+
+]
+
+router.get('/', (req, res) => {
+    const limit = parseInt(req.query.limit)
+    if(!isNaN(limit) && limit > 0) {
+        return res.status(200).json(posts.slice(0, limit));
+    }
+    res.status(200).json(posts);
+    
+
+})
+
+// get single post
+router.get('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => post.id === id);
+
+    if(!post){
+        return res.status(404).json({msg: `A post with id ${id} does not exist`});
+    }
+    
+    res.status(200).json(post);
+    
+})
+
+module.exports = router;
+
+
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const posts = require('./routes/posts');
 const path = require('path');
 require('dotenv').config();
 
@@ -7,12 +8,6 @@ require('dotenv').config();
 const port = process.env.PORT || 8000
 
 
-let posts = [
-    {id: 1, title: 'Post 1', content: 'This is post 1'},
-    {id: 2, title: 'Post 2', content: 'This is post 2'},
-    {id: 3, title: 'Post 3', content: 'This is post 3'}
-
-]
 //app.use(express.static(path.join(__dirname, 'public')));
 
 
@@ -21,23 +16,8 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 
-// get all posts
-app.get('/api/posts', (req, res) => {
-    const limit = parseInt(req.query.limit)
-    if(!isNaN(limit) && limit > 0) {
-        res.status(200).json(posts.slice(0, limit));
-    }else{
-        res.status(200).json(posts);
-    }
-
-})
-
-// get single post
-app.get('/api/posts/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const post = posts.find((post) => post.id === id);
-    res.status(200).json(post);
-})
+// Routes
+app.use('/api/posts', posts);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
