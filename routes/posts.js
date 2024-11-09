3872f39e9ece2c5e834c2b92e507b68d15e19ec2
Hamza Oumeziane <2226562@bdeb.qc.ca>
2024-11-09 11:04:00 -0500

    using ES modules (changing the imports/exports type)

--- routes/posts.js
+++ routes/posts.js
@@ -1,4 +1,4 @@
-const express = require('express');
+import express from 'express';
 
 const router = express.Router();
 
@@ -33,6 +33,6 @@ router.get('/:id', (req, res) => {
     
 })
 
-module.exports = router;
+export default router;
 
 
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const app = express();
-const posts = require('./routes/posts');
-const path = require('path');
-require('dotenv').config();
+import express from 'express';
+import path from 'path';
+import posts from './routes/posts.js';
+import dotenv from 'dotenv'
 
+const app = express();
+dotenv.config();
 
 const port = process.env.PORT || 8000
 
