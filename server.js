c406fafc0e367f260b20c85913a225ed0fc0d8c0
Hamza Oumeziane <2226562@bdeb.qc.ca>
2024-11-09 10:45:35 -0500

    params and query

--- server.js
+++ server.js
@@ -7,28 +7,36 @@ require('dotenv').config();
 const port = process.env.PORT || 8000
 
 
-
-
 let posts = [
     {id: 1, title: 'Post 1', content: 'This is post 1'},
     {id: 2, title: 'Post 2', content: 'This is post 2'},
     {id: 3, title: 'Post 3', content: 'This is post 3'}
 
 ]
-// setup static folder
 //app.use(express.static(path.join(__dirname, 'public')));
+
+
+// setup static folder
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 
+// get all posts
 app.get('/api/posts', (req, res) => {
-    res.json(posts);
+    const limit = parseInt(req.query.limit)
+    if(!isNaN(limit) && limit > 0) {
+        res.status(200).json(posts.slice(0, limit));
+    }else{
+        res.status(200).json(posts);
+    }
+
 })
 
+// get single post
 app.get('/api/posts/:id', (req, res) => {
-    const id = req.params.id;
-    const post = posts.find(post => post.id == id);
-    res.send(post);
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => post.id === id);
+    res.status(200).json(post);
 })
 
 app.listen(port, () => {
